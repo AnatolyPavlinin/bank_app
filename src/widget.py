32a327cdb0255b0800e7340317628aad3d5973d9
Anatolii_Pavlinin <rider3766@yandex.ru>
2025-12-25 18:40:33 +0300

    adding get_date

--- src/widget.py
+++ src/widget.py
@@ -12,3 +12,9 @@ def mask_account_card(inf_account_card: str) -> str:
         mask_card = get_mask_card_number(number_card)
         name_bank = inf_account_card[:-16]
         return f"{name_bank}{mask_card}"
+
+
+def get_date(get_date: str) -> str:
+    """Функция меняет формат даты"""
+    correct_date = get_date[8:10] + "." + get_date[5:7] + "." + get_date[:4]
+    return correct_date
