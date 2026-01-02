e94bc839ff5a1abd34555539f8ed5647eefb7536
Anatolii_Pavlinin <rider3766@yandex.ru>
2026-01-02 14:11:12 +0300

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -6,7 +6,7 @@ def filter_by_state(list_of_dictionaries: list, state: str = "EXECUTED") -> Iter
     (по умолчанию 'EXECUTED'). Функция возвращает новый список словарей, содержащий только те
     словари, у которых ключ state соответствует указанному значению."""
     filtered_list = []
-    state_upper = state.upper()   # Приводим к верхнему регистру
+    state_upper = state.upper()  # Приводим к верхнему регистру
 
     for dictionary in list_of_dictionaries:
         if "state" in dictionary:
@@ -14,3 +14,15 @@ def filter_by_state(list_of_dictionaries: list, state: str = "EXECUTED") -> Iter
             if isinstance(state_value, str) and state_value.upper() == state_upper:
                 filtered_list.append(dictionary)
     return filtered_list
+
+
+def sort_by_date(list_of_dictionaries: Iterable, decreasing: bool = True) -> Iterable:
+    """Функция, которая принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание). Функция должна возвращать
+     новый список, отсортированный по дате (date)."""
+    sorted_list_by_data = sorted(
+        list_of_dictionaries,
+        key=lambda list_of_dic: int(list_of_dic["date"].split("T")[0].replace("-", "")),
+        reverse=decreasing,
+    )
+    return sorted_list_by_data
