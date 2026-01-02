7c8826f65598853df99ace09c0bb9e6a81e607bf
Anatolii_Pavlinin <rider3766@yandex.ru>
2026-01-02 15:46:02 +0300

    docstring fixes

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import Iterable
 
 
-def filter_by_state(list_of_dictionaries: list, state: str = "EXECUTED") -> Iterable:
+def filter_by_state(list_of_dictionaries: Iterable[dict], state: str = "EXECUTED") -> Iterable:
     """Функция, которая принимает список словарей и опционально значение для ключа state
     (по умолчанию 'EXECUTED'). Функция возвращает новый список словарей, содержащий только те
     словари, у которых ключ state соответствует указанному значению."""
@@ -16,9 +16,9 @@ def filter_by_state(list_of_dictionaries: list, state: str = "EXECUTED") -> Iter
     return filtered_list
 
 
-def sort_by_date(list_of_dictionaries: Iterable, decreasing: bool = True) -> Iterable:
+def sort_by_date(list_of_dictionaries: Iterable[dict], decreasing: bool = True) -> Iterable:
     """Функция, которая принимает список словарей и необязательный параметр,
-    задающий порядок сортировки (по умолчанию — убывание). Функция должна возвращать
+    задающий порядок сортировки (по умолчанию — убывание). Функция возвращает
      новый список, отсортированный по дате (date)."""
     sorted_list_by_data = sorted(
         list_of_dictionaries,
